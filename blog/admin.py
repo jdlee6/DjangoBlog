d115070410c88d3e2d3b0c94b79700d8cf179f62
Joe Lee <jdlee6@buffalo.edu>
2019-07-20 20:38:38 -0400

    database + migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# import Post from cwd's models module
+from .models import Post
 
-# Register your models here.
+# register our Post model to the admin site
+admin.site.register(Post)
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-# create dummy data, posts
-posts = [
-    {
-            'author': 'CoreyMS',
-            'title': 'Blog Post 1',
-            'content': 'First post content',
-            'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    },
-]
+# models is in the same directory which is why we can use .
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
