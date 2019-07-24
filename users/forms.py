eb2d093b02ea215569db53d4a94c7ab333f26c9b
Joe Lee <jdlee6@buffalo.edu>
2019-07-23 21:46:43 -0400

    updating user profile

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+# import Profile model to work with it
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
-    # create email field
-    # takes an argument called required which would be set equal to a Boolean (True or False)
-    # by default, required is set to True
     email = forms.EmailField()
-
-    # gives us a nested namespace for configurations and keeps the configurations in one place
-    # within the configuration, we're saying that the model that will be affected is the User model (for example if we do a form.save() it will save it to this user model)
-    # the fields that we have in the list are the fields that we want in the form and in what order
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,15 +1,26 @@
 from django.db import models
-# import User model
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
-    # creates a 1:1 relationship with the existing User model
-    # on_delete=models.CASCADE argument tells django what will happen to the Profile when a user is deleted
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # image field
-    # default picture = default.jpg default image for any user
-    # upload_to='profile_pics' refers to the directory where images will be stored when uploaded
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # this save method will get run after our model is saved
+    # the save method already exists that's why we need to OVERRIDE it with the same name and added functionality
+    def save(self):
+        # run the save method of our parent class
+        super().save()
+        
+        # opens image of the current instance
+        img = Image.open(self.image.path)
+        # if image height/width is greater than 300 pixels
+        if img.height > 300 or img.width > 300:
+            # image dimensions into 300 x 300
+            output_size = (300, 300)
+            # resizes the image
+            img.thumbnail(output_size)
+            img.save(self.image.path)
